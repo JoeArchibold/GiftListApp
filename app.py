f4ed8a300f8c480ae43d54b9fb4343be3be26cc3
jarchibold <jarchibold@mines.edu>
2022-12-06 21:22:59 -0700

    Database mostly working (and safe maybe)

--- app.py
+++ app.py
@@ -1,89 +1,115 @@
 from flask import Flask, render_template, request, escape, jsonify
 from pymongo import MongoClient
+import json
+import os
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb+srv://jarchibold:6TeruB45RowBDB9d@giftlistdevdb.edublop.mongodb.net/?retryWrites=true&w=majority")
+with open('secrets/passwords.txt') as f:
+    password = f.read()
+
+connection_string = f'mongodb+srv://jarchibold:{password}@giftlistdevdb.edublop.mongodb.net/?retryWrites=true&w=majority'
+
+client = MongoClient(connection_string)
 
 db = client["GiftListDevDB"]
 
-lists = []
-
-class List:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-        self.items = []
-
-class Item:
-    def __init__(self, name):
-        self.name = name
-        self.isChecked = False
-
-
 def generateLists():
     returnList = []
+
+    #db.drop_collection("lists")
     # for i in range(100):
-    #     listToAdd = List(i, f'List {i}')
+    #     listToAdd = {
+    #         "_id": i,
+    #         "name": f'List {i}', 
+    #         "items": []
+    #     }
     #     for j in range(5):
-    #         itemToAdd = Item(f'Item {i*5+j}')
-    #         listToAdd.items.append(itemToAdd)
+    #         itemToAdd = {
+    #             "name": f'Item {j}',
+    #             "isChecked": False
+    #         }
+    #         listToAdd['items'].append(itemToAdd)
     #     returnList.append(listToAdd)
     
     # db.create_collection("lists")
-    # for i,list in enumerate(returnList):
-    #     db.lists.insert_one(
-    #         {"name": list.name, 
-    #         "id": i}
-    #     ) 
+    # db.lists.insert_many(returnList) 
 
     return returnList
+
+def getList(listId):
+    listId = int(listId)
+    documents = db.lists.find_one({'_id': listId})
+    response = jsonify(documents)
+    curList = json.loads(response.data)
+    return curList
 
 @app.route("/")
 def home():
     documents = list(db.lists.find())
-    lists = jsonify(documents)
-    return lists#render_template("index.html", name="Joe", data=lists)
+    response = jsonify(documents)
+    lists = json.loads(response.data)
+    return render_template("index.html", data=lists)
+
+@app.route('/lists/<listId>/choose')
+def choose(listId):
+    return render_template("Choose.html", listId=listId)
 
 @app.route('/lists/<listId>/owner')
 def listOwner(listId):
-    lists = generateLists()
-    return render_template("listOwner.html", name="Joe", list=lists[int(listId)])
+    curList = getList(listId)
+    return render_template("listOwner.html", list=curList)
 
 @app.route('/lists/<listId>/add', methods=["GET", "POST"])
 def listOwnerAdd(listId):
-    lists = generateLists()
-    curlist = lists[int(listId)]
+    curList = getList(listId)
     if request.method == "POST":
         inString = request.form.get("textAdd").split(',')
 
         for item in inString:
-            curlist.items.append(Item(escape(item).strip()))
+            curList['items'].append({
+                "name": item,
+                "isChecked": False
+            })
 
-        return render_template("listOwner.html", name="Joe", list=curlist)
+        db.lists.update_one({'_id': int(listId)}, {'$set': curList})
 
-    return render_template("listItemAdd.html", name="Joe", list=curlist)
+        return render_template("listOwner.html", list=curList)
+
+    return render_template("listItemAdd.html", list=curList)
 
 @app.route('/lists/<listId>/remove', methods=["GET", "POST"])
 def listOwnerRemove(listId):
-    lists = generateLists()
-    curlist = lists[int(listId)]
+    curList = getList(listId)
     if request.method == "POST":
-        for val,item in enumerate(curlist.items):
-            if request.form.get(item.name) == 'on':
-                curlist.items[val] = None
+        for val,item in enumerate(curList['items']):
+            if request.form.get(item['name']) == 'on':
+                curList['items'][val] = None
 
-        curlist.items = list(filter(None, curlist.items))
+        curList['items'] = list(filter(None, curList['items']))
 
-        return render_template("listOwner.html", name="Joe", list=curlist)
+        db.lists.update_one({'_id': int(listId)}, {'$set': curList})
 
-    return render_template("listItemRemove.html", name="Joe", list=curlist)
+        return render_template("listOwner.html", list=curList)
+
+    return render_template("listItemRemove.html", list=curList)
 
 
-@app.route('/lists/<listId>/guest')
+@app.route('/lists/<listId>/guest', methods=["GET", "POST"])
 def listGuest(listId):
-    lists = generateLists()
-    return render_template("listGuest.html", name="Joe", list=lists[int(listId)])
+    curList = getList(listId)
+    if request.method == "POST":
+        for val,item in enumerate(curList['items']):
+            if request.form.get(item['name']) == 'on':
+                curList['items'][val]['isChecked'] = True
+            else:
+                curList['items'][val]['isChecked'] = False
+
+        db.lists.update_one({'_id': int(listId)}, {'$set': curList})
+
+        return render_template("listGuest.html", list=curList)
+
+    return render_template("listGuest.html", list=curList)
 
 if __name__ == '__main__':
     # run() method of Flask class runs the application

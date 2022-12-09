29fb93a81376726cea330993deb0271645071a1e
JoeArchibold <mojojoey5555@gmail.com>
2022-12-09 11:48:11 -0700

    Lots of changes, better styling, condensed look, all around pretty good

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, escape, jsonify, redirect, url_for, session
 from pymongo import MongoClient
 import json, hashlib, os, secrets
+import bson
 
 app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
@@ -18,21 +19,24 @@ db = client["GiftListDevDB"]
 def generateLists():
     returnList = []
 
-    # for i in range(10):
-    #     listToAdd = {
-    #         "_id": i,
-    #         "name": f'List {i}', 
-    #         "items": []
-    #     }
-    #     for j in range(5):
-    #         itemToAdd = {
-    #             "name": f'Item {j}',
-    #             "isChecked": False
-    #         }
-    #         listToAdd['items'].append(itemToAdd)
-    #     returnList.append(listToAdd)
+    db.lists.delete_many({})
+    for i in range(10):
+        listToAdd = {
+            "_id": i,
+            "name": f'List {i}', 
+            "items": [],
+            "owner": "randomUser"
+        }
+        for j in range(5):
+            itemToAdd = {
+                "name": f'Item {j}',
+                "isChecked": False,
+                "rank": j + 1
+            }
+            listToAdd['items'].append(itemToAdd)
+        returnList.append(listToAdd)
     
-    # db.lists.insert_many(returnList) 
+    db.lists.insert_many(returnList) 
 
     return returnList
 
@@ -66,13 +70,18 @@ def login():
         else:
             return render_template("login.html", failed=True)
 
-
-
     return render_template("login.html", failed=False)
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    session['loggedIn'] = None
+    session['userId'] = None
+
+    return redirect(url_for('login'))
 
 @app.route("/")
 def index():
-    if not session.get('loggedIn') == None:
+    if session.get('loggedIn') != None:
         return redirect(url_for('home'))
     return redirect(url_for("login"))
 
@@ -114,27 +123,51 @@ def createAccount():
 
 @app.route("/lists")
 def home():
+    if session.get('loggedIn') == None:
+        return redirect(url_for("login"))
+    
+    user = db.users.find_one({'_id': bson.ObjectId(session['userId'])})
+
     documents = list(db.lists.find())
     response = jsonify(documents)
     lists = json.loads(response.data)
-    return render_template("index.html", data=lists)
+
+    userLists = []
+    otherLists = []
+    for curlist in lists:
+        if curlist['owner'] == user['username']:
+            userLists.append(curlist)
+        else:
+            otherLists.append(curlist)
+
+    return render_template("index.html", userLists=userLists, otherLists=otherLists, name=user['name']['first'])
 
 @app.route('/lists/create', methods=["GET", "POST"])
 def create():
-    if request.method == "POST":
-        listName = request.form.get("listName")
-        inString = request.form.get("textAdd").split(',')
+    if session.get('loggedIn') == None:
+        return redirect(url_for("login"))
 
-        itemsToAdd = [];
+    if request.method == "POST":
+        user = db.users.find_one({'_id': bson.ObjectId(session['userId'])})
+
+        listName = request.form.get("listName")
+        inString = [str.strip() for str in request.form.get("textAdd").split(',')]
+
+        itemsToAdd = []
+
         for item in inString:
+            if item == "":
+                continue
+
             itemsToAdd.append({
                 "name": item,
-                "isChecked": False
+                "isChecked": False,
+                "rank": len(itemsToAdd)+1
             })
 
         new_id = db.lists.find().sort("_id", -1).limit(1).next()["_id"] + 1
 
-        db.lists.insert_one({'_id': int(new_id),'name': listName , 'items': itemsToAdd})
+        db.lists.insert_one({'_id': int(new_id),'name': listName , 'items': itemsToAdd, 'owner': user['username']})
 
         return home()
 
@@ -142,50 +175,42 @@ def create():
 
 @app.route('/lists/<listId>/choose')
 def choose(listId):
-    return render_template("choose.html", listId=listId)
+    if session.get('loggedIn') == None:
+        return redirect(url_for("login"))
+    
+    user = db.users.find_one({'_id': bson.ObjectId(session['userId'])})
+    curList = getList(listId)
+
+    if curList['owner'] != user['username']:
+        return redirect(f'/lists/{listId}/guest')
+    else:
+        return redirect(f'/lists/{listId}/owner')
+
 
 @app.route('/lists/<listId>/owner')
 def listOwner(listId):
+    if session.get('loggedIn') == None:
+        return redirect(url_for("login"))
+
+    user = db.users.find_one({'_id': bson.ObjectId(session['userId'])})
     curList = getList(listId)
+
+    if curList['owner'] != user['username']:
+        return redirect(f'/lists/{listId}/guest')
+
     return render_template("listOwner.html", list=curList)
-
-@app.route('/lists/<listId>/add', methods=["GET", "POST"])
-def listOwnerAdd(listId):
-    curList = getList(listId)
-    if request.method == "POST":
-        inString = request.form.get("textAdd").split(',')
-
-        for item in inString:
-            curList['items'].append({
-                "name": item,
-                "isChecked": False
-            })
-
-        db.lists.update_one({'_id': int(listId)}, {'$set': curList})
-
-        return render_template("listOwner.html", list=curList)
-
-    return render_template("listItemAdd.html", list=curList)
-
-@app.route('/lists/<listId>/remove', methods=["GET", "POST"])
-def listOwnerRemove(listId):
-    curList = getList(listId)
-    if request.method == "POST":
-        for val,item in enumerate(curList['items']):
-            if request.form.get(item['name']) == 'on':
-                curList['items'][val] = None
-
-        curList['items'] = list(filter(None, curList['items']))
-
-        db.lists.update_one({'_id': int(listId)}, {'$set': curList})
-
-        return render_template("listOwner.html", list=curList)
-
-    return render_template("listItemRemove.html", list=curList)
-
 
 @app.route('/lists/<listId>/guest', methods=["GET", "POST"])
 def listGuest(listId):
+    if session.get('loggedIn') == None:
+        return redirect(url_for("login"))
+
+    user = db.users.find_one({'_id': bson.ObjectId(session['userId'])})
+    curList = getList(listId)
+
+    if curList['owner'] == user['username']:
+        return redirect(f'/lists/{listId}/owner')
+
     curList = getList(listId)
     if request.method == "POST":
         for val,item in enumerate(curList['items']):
@@ -197,6 +222,50 @@ def listGuest(listId):
         db.lists.update_one({'_id': int(listId)}, {'$set': curList})
 
     return render_template("listGuest.html", list=curList)
+
+
+@app.route('/lists/<listId>/edit', methods=["GET", "POST"])
+def listEdit(listId):
+    if session.get('loggedIn') == None:
+        return redirect(url_for("login"))
+
+    user = db.users.find_one({'_id': bson.ObjectId(session['userId'])})
+    curList = getList(listId)
+
+    if curList['owner'] != user['username']:
+        return redirect(f'/lists/{listId}/guest')
+
+    curList = getList(listId)
+
+    if request.method == "POST":
+        for val,item in enumerate(curList['items']):
+            if request.form.get(item['name']) == 'on':
+                curList['items'][val] = None
+
+        curList['items'] = list(filter(None, curList['items']))
+
+        for val,item in enumerate(curList['items']):
+            curList['items'][val]['rank'] = val+1
+
+        inString = [str.strip() for str in request.form.get("textAdd").split(',')]
+
+        for item in inString:
+            if item == "":
+                continue
+
+            curList['items'].append({
+                "name": item,
+                "isChecked": False,
+                "rank": len(curList['items']) + 1
+            })
+        
+        curList['name'] = request.form.get("listName")
+
+        db.lists.update_one({'_id': int(listId)}, {'$set': curList})
+
+        return redirect(f'/lists/{listId}/owner')
+
+    return render_template("listEdit.html", list=curList)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=False, debug=True)
